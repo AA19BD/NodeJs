358f3a6c69f5df55d5d1b7a0aab05e18d65d6b49
Abilay Aytbanov <aitbanov640@gmail.com>
2021-10-23 03:57:06 +0600

    DevEd-NodeJs-Course

---
+++ DevEd/app.js
@@ -0,0 +1,11 @@
+//Node
+const getUserInfo = require("./sayName");
+
+// sayName();
+// console.log(getUserInfo); //object
+
+getUserInfo.sayName();//Hello there
+getUserInfo.sayAddress();//Rom 123A9
+
+console.log(__dirname)///Users/abylayaitbanov/Desktop/NodeJS/DevEd
+console.log(__filename)///Users/abylayaitbanov/Desktop/NodeJS/DevEd/app.js
---
+++ DevEd/sayName.js
@@ -0,0 +1,10 @@
+const sayName = () => {
+  console.log("Hello there");
+};
+
+const sayAddress = () => {
+    console.log("Rom 123A9");
+};
+
+module.exports.sayName = sayName;
+module.exports.sayAddress = sayAddress;
